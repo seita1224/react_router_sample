fe6aa08df82a067114e495dad300cd337eb68e63
seita <seita.1224.ds@outlook.com>
2021-12-13 22:43:48 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Link } from "react-router-dom";
+import { Router } from "./router/Router";
+import "./styles.css";
+
+export default function App() {
+  console.log("app start");
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/Page1">Page1</Link>
+        <br />
+        <Link to="/Page2">Page2</Link>
+        <br />
+      </div>
+      <Router />
+    </BrowserRouter>
+  );
+}
---
+++ src/router/Router.jsx
@@ -0,0 +1,35 @@
+import { Switch, Route } from "react-router-dom";
+
+import { Home } from "../Home";
+import { Page2 } from "../Page2";
+import { page1Routes } from "./Page1Routes";
+
+export const Router = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route
+        path="/Page1"
+        render={({ match: { url } }) => (
+          <Switch>
+            {page1Routes.map((route) => (
+              <Router
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.childlen}
+              </Router>
+            ))}
+          </Switch>
+        )}
+      />
+
+      <Route path="/Page2">
+        <Page2 />
+      </Route>
+    </Switch>
+  );
+};
